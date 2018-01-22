d0c50426f1456dc8521e7a7f32436141419def8b
Daniel Cote <dccote@novajo.ca>
2018-01-22 10:37:26 -0500

    Correction conflit
    Correction du conflit.

--- potato.m
+++ potato.m
@@ -5,15 +5,6 @@ fprintf('Hello World')
 
 fprint('Hi Andreanne, how are you?')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 x = 2+2;
 fprint('what is x?')
-=======
-disp('I am fine and you')
->>>>>>> b6127f965a6e5ed81c933c0ccc8785b94bebe3b5
-
 disp('The cake is a lie')
-
-=======
->>>>>>> parent of 04723f3... math
